88191c9ceca3c2fdde9c70317b27667bedc0667f
Grey Altr <grey.sifr@gmail.com>
2023-01-24 12:44:11 -0800

    NavLinks completed in Navigator.js: still no display on page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Navigator from './components/navigator/Navigator.js';
+import RGB from './components/RGB/RGB.js';
 
 function App() {
-  return <main className="App"></main>;
+  return (
+    <div>
+      <main className="App">
+        <Router>
+          <Route
+            path="/rgb/:r/:g/:b"
+            element={
+              <>
+                <Navigator />
+                <RGB />
+              </>
+            }
+          />
+        </Router>
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/components/navigator/Navigator.js
+++ src/components/navigator/Navigator.js
@@ -3,9 +3,15 @@ import { NavLink } from 'react-router-dom';
 export default function Navigator() {
   return (
     <>
-      <NavLink>Red</NavLink>
-      <NavLink>Green</NavLink>
-      <NavLink>Blue</NavLink>
+      <NavLink className="red" to="/rgb/255/0/0">
+        Red
+      </NavLink>
+      <NavLink className="green" to="/rgb/0/255/0">
+        Green
+      </NavLink>
+      <NavLink className="blue" to="/rgb/0/0/255">
+        Blue
+      </NavLink>
     </>
   );
 }
