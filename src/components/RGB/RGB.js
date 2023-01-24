577eae043c394cc1e53d87c87210afbf036867fc
Grey Altr <grey.sifr@gmail.com>
2023-01-24 12:24:44 -0800

    Params defined and staged in RGB.js

--- src/components/RGB/RGB.js
+++ src/components/RGB/RGB.js
@@ -1,3 +1,13 @@
+import { useParams } from 'react-router-dom';
+
 export default function RGB() {
-  return <div>RGB</div>;
+  const { r, g, b } = useParams();
+
+  return (
+    <>
+      <div className="background" style={{ background: `rgb(${r}, ${g}, ${b})` }}>
+        <section>{`rgb(${r}, ${g}, ${b}`}</section>
+      </div>
+    </>
+  );
 }
